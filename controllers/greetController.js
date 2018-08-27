bab44377ef188585786b12f160e11aacb49ec1a9
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-08-27 17:54:02 +0300

    Add an example of an asynchronous test

--- controllers/greetController.js
+++ controllers/greetController.js
@@ -3,3 +3,12 @@ exports.getGreeting = function getGreeting(context) {
     const name = context.params.query.name;
     return {message: `Hello ${name}`};
 }
+
+exports.asyncGetGreeting = function asyncGetGreeting(context) {
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			const name = context.params.query.name;
+			resolve({message: `Hello ${name}`});
+		}, 100);
+	});
+}
