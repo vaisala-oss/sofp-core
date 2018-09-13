4e03c4fcb7f54b7522412cb0ce2a71a511911762
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-09-13 17:03:50 +0300

    Make link properties other than href optional as they should be

--- src/lib/link.ts
+++ src/lib/link.ts
@@ -2,8 +2,8 @@
 export interface Link {
     // https://raw.githubusercontent.com/opengeospatial/WFS_FES/master/core/openapi/schemas/link.yaml
     href : string;
-    rel : string;
-    type : string;
-    hreflang : string
-    title : string;
+    rel? : string;
+    type? : string;
+    hreflang? : string
+    title? : string;
 };
