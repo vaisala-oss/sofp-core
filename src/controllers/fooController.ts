1bb46fad8e91e361bf45c9c9ff84ce5147c12570
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-09-13 17:07:37 +0300

    Remove exegesis and start working with bare express

--- src/controllers/fooController.ts
+++
@@ -1,4 +0,0 @@
-// This function has the same name as an operationId in the OpenAPI document.
-exports.getFoo = function getFoo(context) {
-    throw new Error('Foo not implemented!');
-}
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,52 +1,32 @@
+import {Server} from './server';
+import {API} from './api';
+
 import * as express from 'express';
-import * as exegesisExpress from 'exegesis-express';
 import * as http from 'http';
-import * as path from 'path';
 
+const serverPort = 3000;
 
-async function createServer() {
-    // See https://github.com/exegesis-js/exegesis/blob/master/docs/Options.md
-    const options = {
-        controllers: path.resolve(__dirname, '../controllers'),
-        controllersPattern: '**/*Controller.@(js|ts)',
-        allowMissingControllers: false
-    };
+let server = new Server();
+// TODO: add backends
 
-    // This creates an exgesis middleware, which can be used with express,
-    // connect, or even just by itself.
-    const exegesisMiddleware = await exegesisExpress.middleware(
-        path.resolve(__dirname, '../../api/hello-world.yaml'),
-        options
-    );
+let api = new API(server, { title: 'SOFP WFS 3.0 server', contextPath: '/sofp' });
 
-    const app = express();
+const app = express();
+// Pretty-print
+app.set('json spaces', 2);
 
-    // If you have any body parsers, this should go before them.
-    app.use(exegesisMiddleware);
+api.connectExpress(app);
 
-    // Return a 404
-    app.use((req, res) => {
-        res.status(404).json({message: `Not found`});
-    });
-
-    // Handle any unexpected errors
-    app.use((err, req, res, next) => {
-        res.status(500).json({message: `Internal error: ${err.message}`});
-    });
-
-    const server = http.createServer(app);
-
-    return server;
-}
-
-
-createServer()
-.then(server => {
-    server.listen(3000);
-    console.log('Listening on port 3000');
-    console.log('Try visiting http://localhost:3000/greet?name=Jason');
-})
-.catch(err => {
-    console.error(err.stack);
-    process.exit(1);
+app.use((req, res) => {
+    res.status(404).json({message: 'Not found'});
 });
+
+app.use((err, req, res, next) => {
+    res.status(500).json({message: 'Internal error: ${err.message}'});
+});
+
+const httpServer = http.createServer(app);
+httpServer.listen(serverPort);
+
+console.log('Listening on port '+serverPort);
+console.log('Try visiting http://localhost:'+serverPort+api.contextPath);
