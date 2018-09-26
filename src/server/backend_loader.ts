19c013f7f8815c429d36d7a25a6f42b29cac4d60
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-09-26 13:17:20 +0300

    Use absolute directories to load backends

--- src/server/backend_loader.ts
+++ src/server/backend_loader.ts
@@ -12,17 +12,16 @@ const getDirectories = source => readdirSync(source).map(name => join(source, na
 
 
 export function load(path : string) : Backend[] {
-    var backendDirectories = getDirectories(path);
-
     const cwd = process.cwd();
+    var backendDirectories = getDirectories(cwd+'/'+path);
 
     const ret = [];
     _.each(backendDirectories, dir => {
         try {
             console.log('Loading backend '+dir);
-            var moduleSpecContents = readFileSync(cwd+'/'+dir+'/package.json');
+            var moduleSpecContents = readFileSync(dir+'/package.json');
             var moduleSpec = JSON.parse(moduleSpecContents);
-            var mod = require(cwd+'/'+dir);
+            var mod = require(dir);
 
             var backendExports = moduleSpec['sofp-backend'];
             if (!_.isArray(backendExports)) {
