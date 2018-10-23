eeb058c3291f5082cd7e7fba34495f9533f2f3b1
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-10-23 10:55:41 +0300

    Add more tests to property filter

--- src/server/filters/property_filter.test.ts
+++ src/server/filters/property_filter.test.ts
@@ -2,17 +2,30 @@ import {PropertyFilterProvider} from './property_filter';
 
 const provider = new PropertyFilterProvider();
 
-test('Test accept', () => {
+test('Test accept (1 parameter)', () => {
     let filter = provider.parseFilter({ query: { ParameterName: 'foo' }});
     let feature = { properties: { ParameterName: 'foo' } };
     expect(filter.accept(feature)).toBeTruthy();
 });
 
-test('Test not accept', () => {
+test('Test not accept (1 parameter)', () => {
     let filter = provider.parseFilter({ query: { ParameterName: 'foo' }});
     let feature = { properties: { ParameterName: 'bar' } };
     expect(filter.accept(feature)).toBeFalsy();
 });
+
+test('Test accept (2 parameters)', () => {
+    let filter = provider.parseFilter({ query: { ParameterName: 'foo', Yikes: 'bar' }});
+    let feature = { properties: { ParameterName: 'foo', Yikes: 'bar' } };
+    expect(filter.accept(feature)).toBeTruthy();
+});
+
+test('Test not accept (1 parameter matches, 1 does not)', () => {
+    let filter = provider.parseFilter({ query: { ParameterName: 'foo', Yikes: 'bar' }});
+    let feature = { properties: { ParameterName: 'bar', Yikes: 'xxx' } };
+    expect(filter.accept(feature)).toBeFalsy();
+});
+
 
 test('Test serialisation', () => {
     let filter = provider.parseFilter({ query: { ParameterName: 'foo' }});
