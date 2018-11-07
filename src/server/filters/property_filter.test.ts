56dbddb8136a9c736a150baad83f179e80ae4adf
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-11-07 14:55:47 +0200

    Add case sensitivity tests to property filter

--- src/server/filters/property_filter.test.ts
+++ src/server/filters/property_filter.test.ts
@@ -3,32 +3,48 @@ import {PropertyFilterProvider} from './property_filter';
 const provider = new PropertyFilterProvider();
 
 test('Test accept (1 parameter)', () => {
-    let filter = provider.parseFilter({ query: { ParameterName: 'foo' }});
-    let feature = { properties: { ParameterName: 'foo' } };
+    let filter = provider.parseFilter({ query: { name: 'foo' }});
+    let feature = { properties: { name: 'foo' } };
     expect(filter.accept(feature)).toBeTruthy();
 });
 
 test('Test not accept (1 parameter)', () => {
-    let filter = provider.parseFilter({ query: { ParameterName: 'foo' }});
-    let feature = { properties: { ParameterName: 'bar' } };
+    let filter = provider.parseFilter({ query: { name: 'foo' }});
+    let feature = { properties: { name: 'bar' } };
     expect(filter.accept(feature)).toBeFalsy();
 });
 
 test('Test accept (2 parameters)', () => {
-    let filter = provider.parseFilter({ query: { ParameterName: 'foo', Yikes: 'bar' }});
-    let feature = { properties: { ParameterName: 'foo', Yikes: 'bar' } };
+    let filter = provider.parseFilter({ query: { name: 'foo', yikes: 'bar' }});
+    let feature = { properties: { name: 'foo', yikes: 'bar' } };
     expect(filter.accept(feature)).toBeTruthy();
 });
 
 test('Test not accept (1 parameter matches, 1 does not)', () => {
-    let filter = provider.parseFilter({ query: { ParameterName: 'foo', Yikes: 'bar' }});
-    let feature = { properties: { ParameterName: 'bar', Yikes: 'xxx' } };
+    let filter = provider.parseFilter({ query: { name: 'foo', yikes: 'bar' }});
+    let feature = { properties: { name: 'bar', yikes: 'xxx' } };
     expect(filter.accept(feature)).toBeFalsy();
 });
 
-
 test('Test serialisation', () => {
-    let filter = provider.parseFilter({ query: { ParameterName: 'foo' }});
+    let filter = provider.parseFilter({ query: { name: 'foo' }});
     
-    expect(filter.asQuery).toBe('ParameterName=foo');
+    expect(filter.asQuery).toBe('name=foo');
+});
+
+test('Test property names are lowercased', () => {
+    let filter = provider.parseFilter({ query: { NAME: 'foo' }});
+    let feature = { properties: { name: 'foo' } };
+    expect(filter.accept(feature)).toBeTruthy();
+});
+
+test('Test property names are lowercased in serialised format', () => {
+    let filter = provider.parseFilter({ query: { NAME: 'foo' }});
+    expect(filter.asQuery).toBe('name=foo');
+});
+
+
+test('Test no property filter for reserved parameter', () => {
+    let filter = provider.parseFilter({ query: { LIMIT: 'foo', foo: 'bar' }});
+    expect(filter.asQuery).toBe('foo=bar');
 });
