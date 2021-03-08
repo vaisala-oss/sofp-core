a1e98c9c843ccbba20354b6e603eb8104909230e
Sampo Savolainen <sampo.savolainen@spatineo.com>
2021-03-08 17:11:49 +0200

    Add check that limit needs to be at least 0

--- src/server/api.ts
+++ src/server/api.ts
@@ -270,8 +270,11 @@ export class API {
                     }
                 }
 
-		if (req.query.limit !== undefined && req.query.limit > 10000) {
-                    return res.status(400).send('limit should not be greater than 10000');
+		if (req.query.limit !== undefined && Number(req.query.limit) < 1) {
+                    return res.status(400).send('limit should be greater then 0');
+		}
+		if (req.query.limit !== undefined && Number(req.query.limit) > 10000) {
+                    return res.status(400).send('limit should not be greater then 10000');
 		}
 
                 // Check that property filters are in-line with schema (requirement /req/core/query-param-unknown)
